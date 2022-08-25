02636c2b11496a381290d43e629fd14684aaab22
patilsuraksha299 <patilsuraksha299@gmail.com>
2022-08-25 18:43:21 +0530

    first commit

---
+++ p1/app1/urls.py
@@ -0,0 +1,9 @@
+from atexit import register
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('register',views.register,name="register"),
+    path('login',views.login,name="login"),
+    path('logout',views.logout,name="logout"),
+]
